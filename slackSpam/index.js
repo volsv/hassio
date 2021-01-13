ee0a68c172488c976c22bf2643f5b4b634308403
Albert <contact@pixeldublu.ro>
2021-01-14 00:54:52 +0200

    more debug

--- slackSpam/index.js
+++ slackSpam/index.js
@@ -16,9 +16,10 @@ const messages = fs.readFileSync(messagesTextFileName).toString().split(/\r?\n/)
 
 (async () => {
     const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
-    const page = await browser.newPage();
+    const page = await browser.newPage(); 
+    console.log('OPNENING SLACK');
     await page.goto(argv.slackUrl);
-
+    console.log('LOGGING IN');
     await page.waitForSelector('#onetrust-accept-btn-handler', {timeout: 0});
     await page.focus('#email');  
     await page.keyboard.type(email);
@@ -27,10 +28,11 @@ const messages = fs.readFileSync(messagesTextFileName).toString().split(/\r?\n/)
     await page.click('#signin_btn');
     await page.waitForSelector('.p-download_modal__close', {timeout: 0});
     await page.click('.p-download_modal__close');
-
+    console.log('OPENING PRIVATE CHAT');
     await page.goto(argv.slackChatUrl);
 
     for (const message of messages) {
+        console.log('SPAMMING');
         await sendMessage(page, message);
         await sleep(36000);
     }
