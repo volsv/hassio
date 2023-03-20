76465c97d7e75bf3920e1dce9dcb0de134955dd3
volsv <svolsv@gmail.com>
2023-03-20 22:43:45 +0300

    Update run.sh

--- AM43/run.sh
+++ AM43/run.sh
@@ -7,5 +7,5 @@ MQTT_USER="$(jq --raw-output '.mqttUser' $CONFIG_PATH)"
 MQTT_PASS="$(jq --raw-output '.mqttPassword' $CONFIG_PATH)"
 MAC1="$(jq --raw-output '.macAddress1' $CONFIG_PATH)"
 MAC2="$(jq --raw-output '.macAddress2' $CONFIG_PATH)"
-devices ['0283a48e2668', '020062a7c79e']
+devices: ['0283a48e2668', '020062a7c79e']
 node node_modules/.bin/am43ctrl devices [{@}]  -l 3001 -d --url "$MQTT_HOST" -u "$MQTT_USER" -p "$MQTT_PASS" -d
